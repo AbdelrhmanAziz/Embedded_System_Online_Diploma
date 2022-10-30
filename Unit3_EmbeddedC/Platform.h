ad09cb15cae0d76bb234b5fe8b5dbedaf99feafe
AbdelrhmanAziz <abdoelmanzlawey10@gmail.com>
2022-10-30 11:17:35 +0200

    unit3

---
+++ Unit3_EmbeddedC/Platform.h
@@ -0,0 +1,36 @@
+#ifndef PLATFORM_H_
+#define PLATFORM_H_
+
+typedef signed char        int8_t;
+typedef unsigned char      uint8_t;
+
+typedef short              int16_t;
+typedef unsigned short     uint16_t;
+
+typedef int                int32_t;
+typedef unsigned int       uint32_t;
+
+typedef long long          int64_t;
+typedef unsigned long long uint64_t;
+
+typedef float              float32_t;
+typedef double             float64_t;
+
+typedef volatile int8_t    vint8_t;
+typedef volatile uint8_t   vuint8_t;
+
+typedef volatile int16_t   vint16_t;
+typedef volatile uint16_t  vuint16_t;
+
+typedef volatile int32_t   vint32_t;
+typedef volatile uint32_t  vuint32_t;
+
+typedef volatile int64_t   vint64_t;
+typedef volatile uint64_t  vuint64_t;
+
+typedef volatile float32_t vfloat32_t;
+typedef volatile float64_t vfloat64_t;
+
+
+
+#endif /* PLATFORM_H_ */
